4f92183b0bcf3d0ebf21f10fa1d4caa3fc390724
Zave00 <xamartin1900@gmail.com>
2026-06-25 18:41:43 -0400

    Adds double jump

--- Scenes/Entities/player.gd
+++ Scenes/Entities/player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
-@export var jump_force: int
-@export var gravity: int
-@export var move_speed: int
+@export var jump_force: int = 500
+@export var gravity: int = 2000
+@export var move_speed: int = 300
+
+var has_air_step: bool = true
+var jump_count: int = 0
 var direction: float
 
 func apply_gravity(delta: float):
@@ -10,8 +13,23 @@ func apply_gravity(delta: float):
 	
 func get_input():
 	direction = Input.get_axis("left", "right")
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y -= jump_force
+	if Input.is_action_just_pressed("jump"):
+		if has_air_step == true:
+			if jump_count == 0:
+				if is_on_floor():
+					velocity.y -= jump_force
+					jump_count = 1
+				else:
+					velocity.y = 0
+					velocity.y -= jump_force
+					jump_count = 2
+			elif jump_count == 1:
+				velocity.y = 0
+				velocity.y -= jump_force
+				jump_count = 2
+		else:
+			if is_on_floor():
+				velocity.y -= jump_force
 		
 	
 
@@ -19,4 +37,6 @@ func _physics_process(delta: float) -> void:
 	get_input()
 	apply_gravity(delta)
 	velocity.x = direction * move_speed
+	if is_on_floor():
+		jump_count = 0
 	move_and_slide()
